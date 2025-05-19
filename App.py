79958ba3e2f8cd9b4294c873665947a93a374b79
ArthurMaquinImt <arthur.maquin@imt-atlantique.net>
2025-05-19 11:50:02 +0200

    Update: redesign app_handler

--- App.py
+++ App.py
@@ -6,14 +6,17 @@ from pygame._sdl2.video import Texture, Image as Image
 
 
 class BaseSprite2:
-    def __init__(self, app_handler, rect, group):
-        self.app_handler = AppHandler2() # app_handler
+    def __init__(self, app_handler, group, size):
+        self.app_handler = app_handler
         self.image = None
-        self.rect = rect
+        self.rect = None
         self.entity_x, self.entity_y = 0, 0
         self.x, self.y = 0, 0
+        self.width, self.height = size
         self.in_sprite_list = False
         self.group = group
+        # todo later:
+        # self.is_moving = True
 
     def update(self):
         """ Update the image on the screen, not the object. """
@@ -28,7 +31,7 @@ class BaseSprite2:
     def load_on_screen(self):
         if self.image is not None and self.rect is not None:
             if self.entity_x is not None and self.entity_y is not None:
-                self.app_handler.in_group.add_internal(self)
+                self.group.add_internal(self)
                 self.in_sprite_list = True
         else:
             raise Exception("Can't load sprite on screen")
@@ -36,27 +39,30 @@ class BaseSprite2:
     def unload_from_screen(self):
         if self.in_sprite_list:
             self.in_sprite_list = False
-            self.app_handler.in_group.spritedict.pop(self, None)
+            self.group.spritedict.pop(self, None)
             self.image = None
 
 
 class ChunkGroup(BaseSprite2):
-    def __init__(self, app_handler, rect):
+    def __init__(self, app_handler, size):
         group = app_handler.chunk_group
-        super().__init__(app_handler, rect, group)
+        super().__init__(app_handler, group, size)
 
 
 class EntityGroup(BaseSprite2):
-    def __init__(self, app_handler, rect):
+    def __init__(self, app_handler, size):
         group = app_handler.entity_group
-        super().__init__(app_handler, rect, group)
+        super().__init__(app_handler, group, size)
 
 
 class AppHandler2:
-    def __init__(self):
+    def __init__(self, app):
+        self.app = app
+
+        # coordinates:
         self.coord_x, self.coord_y = 0, 0
         self.width, self.height = WIN_W, WIN_H
-        self.zoom = 1
+        self.zoom_index = 0
         self.screen_x, self.screen_y = 0, 0
         self.margin = 50
         self.detection_range = 1000
@@ -64,23 +70,168 @@ class AppHandler2:
         self.detection_y_start = self.coord_y - self.detection_range
         self.detection_x_end = self.coord_x + self.detection_range
         self.detection_y_end = self.coord_y + self.detection_range
+        self.cam_speed = 1.0
+        self.cam_x_retenue = 0
+        self.cam_y_retenue = 0
+
+        # Map:
+        self.map = Map(self)
+        # load this when in detection zone:
+        self.detectable_chunks = []
+        # create its sprite when in visible zone:
+        self.visible_chunks = []
+
+        # Graphics:
+        self.chunk_group = pg.sprite.Group()
+        """ todo later:
+        self.floor_group = pg.sprite.Group()
+        self.object_group = pg.sprite.Group()
+        self.atmosphere_group = pg.sprite.Group()
+        """
+        self.sprite_lock = threading.Lock()
+
+        # Tests:
+        self.min_fps = 9999
+        self.number_of_loaded_sprites = 0
 
     def set_screen_position(self):
         self.screen_x = self.coord_x - self.width / 2
         self.screen_y = self.coord_y - self.height / 2
 
-    def set_screen_size(self, zoom):
-        self.zoom = zoom
-        self.width, self.height = self.width // self.zoom, self.height // self.zoom
+    def set_screen_size(self):
+        zoom = self.get_zoom()
+        width, height = self.width * zoom, self.height * zoom
+        self.app.renderer.scale(width, height)
 
     def zoom_in(self):
-        self.set_screen_size(self.zoom + 0.1)
+        if self.zoom_index > -4:
+            self.zoom_index += 1
+            self.set_screen_size()
 
     def zoom_out(self):
-        if self.zoom > 0.4:
-            self.set_screen_size(self.zoom - 0.1)
+        if self.zoom_index < 4:
+            self.zoom_index -= 1
+            self.set_screen_size()
+
+    def get_zoom(self):
+        zoom_coefficient = 1.2
+        return zoom_coefficient ** -self.zoom_index
+
+    def update_zone(self):
+        self.detection_x_start = self.coord_x - self.detection_range
+        self.detection_y_start = self.coord_y - self.detection_range
+        self.detection_x_end = self.coord_x + self.detection_range
+        self.detection_y_end = self.coord_y + self.detection_range
+        zoom = self.get_zoom()
+        self.screen_x = self.coord_x - (self.width * zoom) / 2
+        self.screen_y = self.coord_y - (self.height * zoom) / 2
+
+    def load_chunks(self):
+        pass
+
+    def get_cam_shift(self, axe, way):
+        """
+        :param axe: x or y
+        :param way: equals -1 if up or left, and equals +1 if down or right
+        :return: The shift of the camera as an integer.
+        """
+        if axe == "x":
+            total = self.cam_x_retenue + (self.cam_speed * way)
+            shift = int(total)
+            shift = total - shift
+        else:
+            total = self.cam_y_retenue + (self.cam_speed * way)
+            shift = int(total)
+            self.cam_y_retenue = total - shift
+        return shift
+
+    def interact(self):
+        # Camera movement:
+        if 'up' in game_app.keybind:
+            self.coord_x += self.get_cam_shift("y", 0)
+        if 'down' in game_app.keybind:
+            self.coord_y += self.get_cam_shift("y", 1)
+        if 'left' in game_app.keybind:
+            self.coord_x += self.get_cam_shift("x", 0)
+        if 'right' in game_app.keybind:
+            self.coord_y += self.get_cam_shift("x", 1)
+
+        # Camera zoom:
+        if 'mouse_up' in game_app.keybind:
+            self.zoom_in()
+        if 'mouse_down' in game_app.keybind:
+            self.zoom_out()
+
+    def sort_sprite_group(self, group):
+        if group.spritedict:
+            sorted_in_group_x = {sprite: value for sprite, value in sorted(group.spritedict.items(),
+                                                                           key=lambda item: item[0].x)}
+            sorted_in_group = {sprite: value for sprite, value in sorted(sorted_in_group_x.items(),
+                                                                         key=lambda item: item[0].y)}
+            group.spritedict = sorted_in_group
+
+    def update(self):
+        pass
+
+    def draw(self):
+        # Draw only the visible group
+        self.chunk_group.draw(self.app.renderer)
+
+    def get_coordinates(self):
+        return self.coord_x, self.coord_y
+
+    def draw_information(self):
+        if self.app.clock.get_fps() < self.min_fps and self.app.clock.get_fps() != 0.0:
+            self.min_fps = self.app.clock.get_fps()
+        min_fps = normalize_text(str(self.min_fps), 4)
+        total_tiles = normalize_text(str(self.number_of_loaded_sprites))
+        fps = normalize_text(str(self.app.get_fps()))
+        cam_coord = normalize_text(f"x: {self.coord_x} | y: {self.coord_y}", 24)
+        test_timer = ""#self.map.manager.test_timer
+        print(f"\rsprites: {total_tiles}; minimum FPS: {min_fps}; fps: {fps}; cam_coord: {cam_coord}; max temp: {normalize_text(str(self.function_timing_result))}; text_timer: {test_timer}", end='')
+
+    def stop_all_threads(self):
+        self.map.manager.stop()
 
 
+class AppInformation:
+    def __init__(self, app_handler):
+        self.app_handler = app_handler
+        self.sprite_count = 0
+        self.fps = 0
+        self.min_fps = 9999
+        self.total_tiles = 0
+
+    def update_fps(self):
+        fps = self.app_handler.app.get_fps()
+        if fps != 0:
+            self.fps = fps
+            if fps < self.min_fps:
+                self.min_fps = fps
+
+    def update_sprite_count(self):
+        total = 0
+        group_list = (g_list for name, g_list in self.app_handler.items() if "group" in name)
+        for group in group_list:
+            total += len(group)
+        self.sprite_count = total
+
+    def update_information(self):
+        self.update_fps()
+        self.update_sprite_count()
+
+    def print_info(self, tiles = True, fps = True, minimum_fps = True, coordinates = True):
+        info_list = []
+        if tiles:
+            info_list.append(f"Tiles count: {normalize_text(self.total_tiles)}; ")
+        if fps:
+            info_list.append(f"FPS: {normalize_text(self.fps)}; ")
+        if minimum_fps:
+            info_list.append(f"Minimum FPS: {normalize_text(self.min_fps)}; ")
+        if coordinates:
+            info_list.append(f"Coordinates: {normalize_text(self.app_handler.get_coordinates())}; ")
+        infos = "".join(info_list)
+        print(f"\r{infos}", end='')
 
 
 class BaseSprite:
@@ -142,7 +293,7 @@ class AppHandler:
         self.pause_group = {}
         self.param_hud = pg.Surface([400, 200])
         self.font = ft.SysFont('Verdana', FONT_SIZE)
-        self.tick_divider = 0
+        self.tick_counter = 0
         self.number_of_loaded_sprites = 0
         self.zoom_scale = 5
         self.cam_speed = BASE_SPEED
@@ -369,7 +520,7 @@ class App:
         self.fps.append(self.clock.get_fps())
         if len(self.fps) == FPS_LIST_SIZE:
             self.fps.pop(0)
-        if not self.fps:
+        else:
             return 0
         return sum(self.fps) / len(self.fps)
 
--- test_tools.py
+++ test_tools.py
@@ -18,6 +18,9 @@ def print_time(interval, tag="default", threshold = 0.05, information=""):
     return False
 
 def normalize_text(text, size=8):
+    var_type = type(text)
+    if var_type != str:
+        text = str(text)
     text_size = len(text)
     diff = abs(size - text_size)
     if text_size < size:
